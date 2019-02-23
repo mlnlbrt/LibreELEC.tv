02669c07f37e49c1af35dd2b9abbe9fe41016010
Albert Malina <albert.malina@gmail.com>
2019-02-23 21:09:17 +0100

    Add smartmontools package

---
+++ packages/sysutils/smartmontools/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="smartmontools"
+PKG_VERSION="7.0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://smartmontools.sourceforge.net"
+PKG_URL="https://downloads.sourceforge.net/sourceforge/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="system"
+PKG_SHORTDESC="smartmontools: control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives"
+PKG_LONGDESC="Control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
